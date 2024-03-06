256c907a9c814c953e4060a9be50ff091281313c
Michael <hnthu.aung@gmail.com>
2024-03-06 10:34:45 +0630

    dynamic route complete

--- app/Services/RouteService.php
+++ app/Services/RouteService.php
@@ -4,19 +4,49 @@ namespace App\Services;
 class RouteService{
     private static $routes = [];
 
-    public function callCorrespondentController($request,$http_method){
-        if(array_key_exists($request,self::$routes)){
-            if(self::$routes[$request]['http_method'] === $http_method){
-                $obj_name = self::$routes[$request]['controller'];
-                $obj_method_name = self::$routes[$request]['controller_method_name'];
-                $controller = new $obj_name();
-                $result = $controller->$obj_method_name();
+    // allCorrespondentController() => version one
+    // public function callCorrespondentController($request,$http_method){
+    //     if(array_key_exists($request,self::$routes)){
+    //         if(self::$routes[$request]['http_method'] === $http_method){
+    //             $obj_name = self::$routes[$request]['controller'];
+    //             $obj_method_name = self::$routes[$request]['controller_method_name'];
+    //             $controller = new $obj_name();
+    //             $result = $controller->$obj_method_name();
 
-                //freeing the obj (I hope so!)
-                unset($controller);
-                return $result;
+    //             //freeing the obj (I hope so!)
+    //             unset($controller);
+    //             return $result;
+    //         }
+    //     }
+    // }
+
+    public function callCorrespondentController($request,$http_method){
+        foreach(self::$routes as $key => $value){
+            if(preg_match($key,$request)){
+                if($value['http_method'] === $http_method){
+                    $controller_name = $value['controller'];
+                    $controller_method_name = $value['controller_method_name'];
+
+                    $obj = new $controller_name();
+                    $result = $obj->$controller_method_name();
+
+                    //freeing the obj (I hope so!)
+                    unset($obj);
+                    return $result;
+                }
             }
         }
+    }
+
+
+
+
+    private static function setRegexRoute($uri){
+
+        $strReplace = str_replace('/','\/',$uri);
+        $regex = preg_replace('/[{]+[A-Za-z\d]+[}]+/','[A-Za-z\d]+', $strReplace);
+        $result = '/^'.$regex.'$/';
+        return $result;
     }
     
     private static function setMethod($http_method,$uri,$controller, $controller_method_name){
@@ -28,19 +58,23 @@ class RouteService{
     }
 
     public static function get($uri,$controller,$controller_method_name){
-        self::setMethod("GET",$uri,$controller,$controller_method_name);
+        $regexUri = self::setRegexRoute($uri);
+        self::setMethod("GET",$regexUri,$controller,$controller_method_name);
     }
 
     public static function post($uri,$controller,$controller_method_name){
-        self::setMethod("POST",$uri,$controller,$controller_method_name);
+        $regexUri = self::setRegexRoute($uri);
+        self::setMethod("POST",$regexUri,$controller,$controller_method_name);
     }
 
     public static function put($uri,$controller,$controller_method_name){
-        self::setMethod("PUT",$uri,$controller,$controller_method_name);
+        $regexUri = self::setRegexRoute($uri);
+        self::setMethod("PUT",$regexUri,$controller,$controller_method_name);
     }
 
     public static function delete($uri,$controller,$controller_method_name){
-        self::setMethod("DELETE",$uri,$controller,$controller_method_name);
+        $regexUri = self::setRegexRoute($uri);
+        self::setMethod("DELETE",$regexUri,$controller,$controller_method_name);
     }
 
 
--- public/test.php
+++ public/test.php
@@ -14,6 +14,8 @@ use App\Services\RouteService;
 // echo $userController;
 // die();
 
+$_POST['email'] = 'test@gmail.com';
+$_POST['password'] = 'password';
 RouteService::post("/login",UserController::class,"login");
 $call = new RouteService();
 var_dump($call->callCorrespondentController("/login","POST"));
@@ -24,3 +26,70 @@ var_dump($call->callCorrespondentController("/login","POST"));
 
 // linux test@gmail.com password
 // window admin@gmail.com admin
+
+// $uri = "/profile/show/{id}/something/{id}/something/{jeje}";
+
+// $result = str_replace('/','\/',$uri);
+// echo $result;
+// echo "<br/>";
+
+// $pr = preg_replace('/{[^}]*}/','([^/]+)',$result);
+// $pr = preg_replace('/[{]+[A-Za-z\d]+[}]+/','[A-Za-z\d]+', $result);
+// echo $pr;
+// echo "<br/>";
+// $sr = '/^'.$pr.'$/';
+
+// $incoming = '/profile/show/johndoe/something/20/something/hehehe';
+
+// echo $sr;
+
+// // $sr = '/^\/profile\/show\/[A-Za-z\d]+\/something\/[A-Za-z\d]+\/something\/something+$/';
+// $a = preg_match_all($sr,$incoming,$matches);
+// echo "<br/>";
+
+// print_r($a);
+
+// foreach($matches as $match){
+//     var_dump($matches);
+// }
+
+// $routes = ['hello' => ['a' => 'hello from a', 'b' => 'hello from b', 'c' => 'hello from c']];
+
+// foreach($routes as $key => $value){
+//     echo "<br/>";
+//     var_dump($key);
+//     echo "<br/>";
+//     var_dump($value['a']);
+//     echo "<br/>";
+//     var_dump($value['b']);
+//     var_dump($value['c']);
+// }
+
+//  function callController(){
+//     foreach($routes as $key => $value){
+//         if(preg_match($key,$request)){
+//             if($http_method === $vlaue['http_method'])
+//                 $value['contoller'];
+//                 $value['controller_method_name'];
+//                 return $result;
+//         }
+//     }
+// }
+
+
+// function secondNested($hello){
+//     echo $hello;
+// }
+// function nested($hello){
+//     secondNested($hello);
+// }
+// echo '<br/>';
+// nested('hello world');
+
+
+
+
+
+
+
+
