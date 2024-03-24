d6783e5f087d3544f800aee13cd6ca04765aa591
Michael <hnthu.aung@gmail.com>
2024-03-24 14:46:42 +0630

    login

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ RouteService::post('/login',UserController::class,'login');
 RouteService::get('/',UserController::class,'index');
 RouteService::get('/logout',UserController::class,'logout')->middleware('guest');
 RouteService::get('/dashboard',UserController::class,'dashboard');
+RouteService::get('/register',UserController::class,'index');
--- view/login.php
+++ view/login.php
@@ -1,8 +1,14 @@
-<?php require("components/header.php")?>
+<?php require("components/header.php") ?>
 
-<body class="text-center">
-	<div class="wrap">
-		<h1 class="h3 mb-3">Qwitter</h1>
+
+
+<div style="width: 100%;
+			max-width: 500px;
+			margin: 40px auto;" class="text-center border border-primary-subtle rounded d-flex justify-content-center">
+	<div class="w-75">
+		<h1 class="h3 mb-3">
+			<a href="/" class="text-decoration-none text-black">Qwitter</a>
+		</h1>
 
 		<?php if (isset($_GET['incorrect'])) : ?>
 			<div class="alert alert-warning">
@@ -22,22 +28,17 @@
 			</div>
 		<?php endif ?>
 		<form action="/login" method="post">
-			<!-- <input type="text" name="name" class="form-control mb-2" placeholder="name" required> -->
 			<input type="email" name="email" class="form-control mb-2" placeholder="Email" required>
-
 			<input type="password" name="password" class="form-control mb-2" placeholder="Password" required>
-
 			<button type="submit" class="w-100 btn btn-lg btn-primary">
 				Login
 			</button>
 		</form>
 		<br>
-
-		<a href="register.php">Register</a>
-		
+		<a href="/register">Register</a>
 	</div>
-  <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script> -->
-</body>
+</div>
+
+
 
 <?php require("components/footer.php") ?>
